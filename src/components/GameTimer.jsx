da2ecca7b9c8f5ddb8a185120276ceca568e6a7e
saraoriola <saraoriola@outlook.com>
2023-11-19 18:43:27 +0100

    stepper a mano

--- src/components/GameTimer.jsx
+++ src/components/GameTimer.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const GameTimer = ({ countdown }) => (
-  <div>
-    <p className="countdown">{countdown}</p>
-    <p className="start-message">¡Que comience el juego!</p>
-  </div>
-);
+import handOne from '../assets/hands/uno.png';
+import handTwo from '../assets/hands/dos.png';
+import handThree from '../assets/hands/tres.png';
+
+const numberHands = {
+  1: handOne,
+  2: handTwo,
+  3: handThree,
+};
+
+const GameTimer = ({ countdown }) => {
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    let timeout;
+
+    if (countdown === 0) {
+      timeout = setTimeout(() => {
+        setShowMessage(true);
+      }, 5000); 
+    } else {
+      setShowMessage(false);
+    }
+
+    return () => clearTimeout(timeout);
+
+  }, [countdown]);
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      {countdown > 0 && countdown <= 3 && (
+        <img
+          src={numberHands[countdown]}
+          alt={`Número ${countdown}`}
+          style={{ width: '300px', height: '300px' }}
+        />
+      )}
+
+      {showMessage && (
+        <p className="start-message">¡Que comience el juego!</p>
+      )}
+    </div>
+  );
+};
 
 GameTimer.propTypes = {
   countdown: PropTypes.number.isRequired,
